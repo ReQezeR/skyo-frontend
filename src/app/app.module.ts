0e2a9608a5a4ea7fe7ba6a5f186baeb99c8f0327
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-03-10 21:25:52 +0100

    add modules and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,27 +14,43 @@ import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatPaginatorModule} from '@angular/material/paginator';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
 import { StreamersTableComponent } from './components/streamers-table/streamers-table.component';
+import { NewStreamerDialogComponent } from './components/new-streamer-dialog/new-streamer-dialog.component';
+import { EditStreamerDialogComponent } from './components/edit-streamer-dialog/edit-streamer-dialog.component';
+import { FilterStreamerDialogComponent } from './components/filter-streamer-dialog/filter-streamer-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavbarComponent,
-    StreamersTableComponent
+    StreamersTableComponent,
+    NewStreamerDialogComponent,
+    EditStreamerDialogComponent,
+    FilterStreamerDialogComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
 
     MatTableModule,
     MatSortModule,
     MatIconModule,
     MatButtonModule,
     MatToolbarModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatDialogModule,
+    MatInputModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
