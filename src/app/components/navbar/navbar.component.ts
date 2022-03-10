dc133d31046b71e57e6daa30ffe2f4d38b8b8a04
ReQezeR <44375216+ReQezeR@users.noreply.github.com>
2022-03-10 21:24:56 +0100

    add dialogs support

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DataService } from '@app/services/data.service';
+import { FilterStreamerDialogComponent } from '../filter-streamer-dialog/filter-streamer-dialog.component';
+import { NewStreamerDialogComponent } from '../new-streamer-dialog/new-streamer-dialog.component';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dataService: DataService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
   }
 
+  addStreamerDialog(){
+    const dialogRef = this.dialog.open(NewStreamerDialogComponent,
+      {
+        maxWidth: '1000px',
+        width: '80vw',
+        autoFocus: false,
+        panelClass: 'custom-dialog-container',
+      }
+    );
+    dialogRef.afterClosed().subscribe(result => {
+      // console.log(typeof(result));
+      if(result) {
+        console.log(result);
+        this.dataService.addStreamer(result);
+      }
+    });
+  }
+
+
+  filterStreamerDialog(){
+    const dialogRef = this.dialog.open(FilterStreamerDialogComponent,
+      {
+        maxWidth: '1000px',
+        width: '80vw',
+        autoFocus: false,
+        panelClass: 'custom-dialog-container',
+      }
+    );
+    dialogRef.afterClosed().subscribe(result => {
+      // console.log(typeof(result));
+      if(result) {
+        // console.log(result);
+        this.dataService.filterStreamers(result);
+      }
+    });
+  }
 }
