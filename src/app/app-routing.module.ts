70a26d93caa376030e3e725f10d071fc615d98e1
dmistera2 <dmistera2@gmail.com>
2022-03-11 23:23:40 +0100

    Added buttons on the Navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { StatsComponent } from './pages/stats/stats.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
+  { path: 'stats', component: StatsComponent},
 ];
 
 @NgModule({
